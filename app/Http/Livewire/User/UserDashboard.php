cc49fa84a098726f994a1ae10e9e1da0b53ed922
Bharath <bharathnbs01@gmail.com>
2022-09-15 09:57:32 +0530

    user voting chage the button of list

--- app/Http/Livewire/User/UserDashboard.php
+++ app/Http/Livewire/User/UserDashboard.php
@@ -4,14 +4,25 @@ namespace App\Http\Livewire\User;
 
 use Livewire\Component;
 use App\Models\Candidate;
+use App\Models\Voting;
 
 class UserDashboard extends Component
 {
-    public $candidates;
+    public $candidates, $candidate_id, $user_id;
 
     public function mount()
     {
         $this->candidates = Candidate::get();
+    }
+
+    public function store($id)
+    {
+        // dd($id);
+        Voting::create([
+
+            'candidate_id' =>$id,
+            'user_id' => Auth()->id(),
+        ]);
     }
     public function render()
     {
--- resources/views/livewire/user/user-dashboard.blade.php
+++ resources/views/livewire/user/user-dashboard.blade.php
@@ -1,5 +1,11 @@
 <div>
-    @foreach($candidates as $candidate)
-        <span>{{ $candidate->name }}</span><br>
-    @endforeach
+    <div style="background-color:#F0E9D2; height:100px;">
+        <h2 style="margin-left:39%; margin-bottom:20px; position:absolute; margin-top:30px;">Candidate List</h2>
+    </div>
+    <div style="margin-top:30px;">
+        @foreach($candidates as $candidate)
+            <button wire:click="store({{ $candidate->id }})" class="list-group-item" style="width:300px; padding:20px; margin-left:37%;">{{$candidate->name}}</button>
+        @endforeach
+    </div>
+ 
 </div>
