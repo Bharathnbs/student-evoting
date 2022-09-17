0cb33e848ba6d90a1cdc2dc54e131500e38d7b32
Bharath <bharathnbs01@gmail.com>
2022-09-17 10:47:43 +0530

    winner result card create completed

--- app/Http/Livewire/Admin/WinerReport.php
+++ app/Http/Livewire/Admin/WinerReport.php
@@ -8,15 +8,24 @@ use App\Models\Candidate;
 
 class WinerReport extends Component
 {
-    public $candidates , $voting, $result;
+    public $candidates , $voting, $result = 0, $winner;
 
     public function mount()
     {
         $this->candidates = Candidate::withCount('voting')->get();
+        // dd($this->candidates);
+        foreach($this->candidates as $candidate)
+        {
+            if($this->result < $candidate->voting_count){
+                $this->result = $candidate->voting_count;
+                // echo($candidate->name);
+                $this->winner = $candidate->name;   
+            }
+        }  
     }
 
     public function render()
     {
-        return view('livewire.admin.winer-report');
+        return view('livewire.admin.winer-report')->layout('layouts.admin.voting.result');
     }
 }
--- resources/views/livewire/admin/winer-report.blade.php
+++ resources/views/livewire/admin/winer-report.blade.php
@@ -1,7 +1,7 @@
 <div>
 
     @foreach($candidates as $candidate)
-        <div style="">
+        <div style="margin-top:30px;">
             <div class="name" style="background-color:#F0E9D2; width:30%; height:100px; margin-left:15%; margin-top:5px;">
                 <p style="margin-left:40%; padding-top:30px; font-size:30px;">{{ $candidate->name }}</p>
             </div>
@@ -11,5 +11,9 @@
         </div>
     @endforeach
 
+    <div style="width:500px; height:200px; background-color:#2C786C; margin-left:31%; margin-top:50px;">
+        <p style="color:white; margin-left:20%; padding-top:30px; font-size:30px">Winner Of Candidate</p>
+        <p style="margin-left:35%; color:white; font-size:30px; ">{{ $winner }}</p>
+    </div>
 
 </div>
