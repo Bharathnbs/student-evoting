addad001af380c86f8079b4a76a466a1daaf3557
Bharath <bharathnbs01@gmail.com>
2022-09-17 12:37:44 +0530

    add font in header inset p tag

--- resources/views/layouts/admin/dashboard.blade.php
+++ resources/views/layouts/admin/dashboard.blade.php
@@ -7,6 +7,7 @@
     <title>Admin Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <style>
+        @import url('https://fonts.googleapis.com/css2?family=Oswald:wght@200&display=swap');
         .option:hover{
             width:200px;
             transform:scale(1.2);
--- resources/views/livewire/admin/admin-dashboard.blade.php
+++ resources/views/livewire/admin/admin-dashboard.blade.php
@@ -1,11 +1,11 @@
 <div>
     <div style="width:100%; height:100px; background-color:#F7CCAC;">
-        <p style="margin-left:30%; padding-top:30px; font-size:30px;">KAMARAJAR ARTS AND SCIENCE COLLAGE</p>
+        <p style="margin-left:30%; padding-top:20px; font-size:40px; font-family: 'Oswald', sans-serif;">KAMARAJAR ARTS AND SCIENCE COLLAGE</p>
     </div>
     <div style="margin-top:30px;">
-       <div  class="option" style="width:400px; background-color:#14103B; height:150px; margin-left:100px; border-radius:10px;"><a href="{{ route('admin.userindex')}}" style=" width:50%; height:200px; position:absolute; margin-left:140px; margin-top:55px; font-size:20px; text-decoration:none; color:white">User Page</a></div> 
-        <div class="option" style="width:400px; background-color:#14103B; height:150px; margin-left:600px; margin-top:-150px;  border-radius:10px;"><a href="{{ route('admin.create_candidate')}} "  style=" width:50%; height:200px; position:absolute; margin-left:140px; margin-top:55px; font-size:20px; text-decoration:none; color:white">Create Candidate</a></div>
-        <div class="option" style="width:400px; background-color:#14103B; height:150px; margin-left:1100px; margin-top:-150px;  border-radius:10px;"><a href="{{ route('admin.show_candidate')}} "  style=" width:50%; height:200px; position:absolute; margin-left:140px; margin-top:55px; font-size:20px; text-decoration:none; color:white">Show Candidate</a></div>
-        <div class="option" style="width:400px; background-color:#14103B; height:150px; margin-left:600px; margin-top:50px;  border-radius:10px;"><a href="{{ route('admin.winner_report')}} "  style=" width:50%; height:200px; position:absolute; margin-left:140px; margin-top:55px; font-size:20px; text-decoration:none; color:white">Winner Report</a></div>
+       <div  class="option" style="width:400px; background-color:#14103B; height:150px; margin-left:100px; border-radius:10px;"><a href="{{ route('admin.userindex')}}" style=" position:absolute; margin-left:140px; margin-top:55px; font-size:20px; text-decoration:none; color:white">User Page</a></div> 
+        <div class="option" style="width:400px; background-color:#14103B; height:150px; margin-left:600px; margin-top:-150px;  border-radius:10px;"><a href="{{ route('admin.create_candidate')}} "  style="  position:absolute; margin-left:140px; margin-top:55px; font-size:20px; text-decoration:none; color:white">Create Candidate</a></div>
+        <div class="option" style="width:400px; background-color:#14103B; height:150px; margin-left:1100px; margin-top:-150px;  border-radius:10px;"><a href="{{ route('admin.show_candidate')}} "  style="position:absolute; margin-left:140px; margin-top:55px; font-size:20px; text-decoration:none; color:white">Show Candidate</a></div>
+        <div class="option" style="width:400px; background-color:#14103B; height:150px; margin-left:600px; margin-top:50px;  border-radius:10px;"><a href="{{ route('admin.winner_report')}} "  style="position:absolute; margin-left:140px; margin-top:55px; font-size:20px; text-decoration:none; color:white">Winner Report</a></div>
     </div>
 </div>
