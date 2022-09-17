89f584d40f3775fb4eb586024587e37474251177
Bharath <bharathnbs01@gmail.com>
2022-09-17 09:26:24 +0530

    winner report ui finshed

--- resources/views/livewire/admin/winer-report.blade.php
+++ resources/views/livewire/admin/winer-report.blade.php
@@ -1,12 +1,12 @@
 <div>
 
     @foreach($candidates as $candidate)
-        <div style="background-color:black;">
-            <div class="name" style="background-color:red; width:30%; height:100px; margin-left:15%;">
-                {{ $candidate->name }}
+        <div style="">
+            <div class="name" style="background-color:#F0E9D2; width:30%; height:100px; margin-left:15%; margin-top:5px;">
+                <p style="margin-left:40%; padding-top:30px; font-size:30px;">{{ $candidate->name }}</p>
             </div>
-            <div class="voting-count" style="background-color:blue;  width:30%; height:100px; margin-left:50%;">
-                {{ $candidate->voting_count }}
+            <div class="voting-count" style="background-color:#54436B;  width:30%; height:100px; margin-left:48%; margin-top:-100px; color:white;">
+                <p style="margin-left:45%; padding-top:20px; font-size:40px;">{{ $candidate->voting_count }}</p>
             </div>
         </div>
     @endforeach
